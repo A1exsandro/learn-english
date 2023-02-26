cf81091726cfcced9aae206f40a181417e75e9c4
A1exsandro <alexsandro.acp@gmail.com>
2023-02-26 13:15:21 -0400

    add layout in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
+import Layout from '@/components/layout'
 import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
+
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </MantineProvider>
   )
 }
