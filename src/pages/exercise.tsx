b940d84617b43d1cf45fb53c11726bb08e493663
A1exsandro <alexsandro.acp@gmail.com>
2023-02-27 14:34:39 -0400

    add exercise page

--- src/pages/exercise.tsx
+++ src/pages/exercise.tsx
@@ -1,6 +1,10 @@
-import { Text } from "@mantine/core";
+import { useWords} from "@/contexts/Words"
+import { Text } from "@mantine/core"
 
 export default function Exercise () {
+  const { newWords, setNewWords } = useWords()
+  console.log(newWords)
+
   return (
     <Text>Exercise Page</Text>
   )
