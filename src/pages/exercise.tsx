e7be2f4af33572efe3c2029b85a793ae1757326a
A1exsandro <alexsandro.acp@gmail.com>
2023-02-27 20:15:40 -0400

    exemple of exercise

--- src/pages/exercise.tsx
+++ src/pages/exercise.tsx
@@ -1,11 +1,38 @@
 import { useWords} from "@/contexts/Words"
-import { Text } from "@mantine/core"
+import { Button, Flex, Text } from "@mantine/core"
 
 export default function Exercise () {
   const { newWords, setNewWords } = useWords()
   console.log(newWords)
 
   return (
-    <Text>Exercise Page</Text>
+    <>
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Button>{newWords[0].word}</Button>
+      </Flex>
+
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Button>{newWords[1].translation}</Button>
+        <Button>{newWords[2].translation}</Button>
+        <Button>{newWords[0].translation}</Button>
+        <Button>{newWords[3].translation}</Button>
+      </Flex>
+    </>
   )
 }
