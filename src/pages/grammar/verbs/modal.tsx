1e76f2b48513660f1c8484b0d1a88657c51d6e96
A1exsandro <alexsandro.acp@gmail.com>
2023-03-02 14:39:16 -0400

    content modal

--- src/pages/grammar/verbs/modal.tsx
+++ src/pages/grammar/verbs/modal.tsx
@@ -1,7 +1,41 @@
-import { Text } from "@mantine/core";
+import { Button, Center, Flex, Text } from "@mantine/core";
+import { useState } from "react";
 
 export default function Modal () {
+  const [arrayModal, setArrayModal] = useState<string[]>([
+    'can', 'could', 'must', 'may', 'might', 'should', 'shall', 'will', 'would'
+  ])
+
   return (
-    <Text>Modal Verbs</Text>
+    <>
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Text>Modal Verbs</Text>
+        <Text>{arrayModal.length}</Text>
+      </Flex>
+
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        {
+          arrayModal.map((modal, i) => (
+            <Button key={i}>{modal}</Button>
+          ))
+        }
+      </Flex>
+    </>
   )
 }
