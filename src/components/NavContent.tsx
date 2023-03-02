63a5b7100a82f64bae035c4bee0658a28de92021
A1exsandro <alexsandro.acp@gmail.com>
2023-03-02 16:38:42 -0400

    add scroll area in navbar

--- src/components/NavContent.tsx
+++ src/components/NavContent.tsx
@@ -1,39 +1,70 @@
-import { Flex, Button, NavLink, Box } from '@mantine/core' 
+import { NavLink, Box, ScrollArea } from '@mantine/core' 
 import Link from 'next/link'
 
 export default function NavContent () {  
 
   return (
-    <Box w={130}>
-      <Link style={{textDecoration: 'none'}} href="/">
-        <NavLink label="Home" />
-      </Link>
-
-      <Link style={{textDecoration: 'none'}} href="/english">
-        <NavLink label="English" />
-      </Link>
-
-      <Link style={{textDecoration: 'none'}} href="/exercise">
-        <NavLink label="Exercises" />
-      </Link>
-
-      <Link style={{textDecoration: 'none'}} href="/grammar/article">
-        <NavLink label="Articles" />
-      </Link>
-
-      <Link style={{textDecoration: 'none'}} href="/grammar/noun">
-        <NavLink label="Noun" />
-      </Link>
-
-      <Link style={{textDecoration: 'none'}} href="/grammar/pronoun">
-        <NavLink label="Pronouns" />
-      </Link>
- 
-      <NavLink label="Verb" childrenOffset={28}>
-        <Link style={{textDecoration: 'none'}} href="/grammar/verbs/modal">
-          <NavLink label="Modal Verbs" />
+    <ScrollArea 
+      h={400} 
+      w={130} 
+      scrollbarSize={8} 
+    >
+      <Box w={130}>
+        <Link style={{textDecoration: 'none'}} href="/">
+          <NavLink label="Home" />
         </Link>
-      </NavLink>
+
+        <Link style={{textDecoration: 'none'}} href="/english">
+          <NavLink label="English" />
+        </Link>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/article">
+          <NavLink label="Articles" />
+        </Link>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/noun">
+          <NavLink label="Noun" />
+        </Link>
+
+        <NavLink label="Adjective" childrenOffset={28}>
+          <Link style={{textDecoration: 'none'}} href="/grammar/adjective">
+            <NavLink label="Adjective" />
+          </Link>
+          <Link style={{textDecoration: 'none'}} href="/grammar/numeral">
+          <NavLink label="Numeral" />
+        </Link>
+        </NavLink>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/pronoun">
+          <NavLink label="Pronouns" />
+        </Link>
+  
+        <NavLink label="Verb" childrenOffset={28}>
+          <Link style={{textDecoration: 'none'}} href="/grammar/verbs/modal">
+            <NavLink label="Modal Verbs" />
+          </Link>
+        </NavLink>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/adverb">
+          <NavLink label="Adverb" />
+        </Link>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/preposition">
+          <NavLink label="Preposition" />
+        </Link>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/conjunction">
+          <NavLink label="Conjunction" />
+        </Link>
+
+        <Link style={{textDecoration: 'none'}} href="/grammar/interjection">
+          <NavLink label="Interjection" />
+        </Link>
+
+        <Link style={{textDecoration: 'none'}} href="/exercise">
+          <NavLink label="Exercises" />
+        </Link>
       </Box>
+    </ScrollArea>
   )
 }
