7d9d2ca9470255fb80bc43aa438199364667572c
A1exsandro <alexsandro.acp@gmail.com>
2023-03-02 14:50:34 -0400

    content pronoun

--- src/pages/grammar/pronoun.tsx
+++ src/pages/grammar/pronoun.tsx
@@ -1,7 +1,41 @@
-import { Text } from "@mantine/core"
+import { Button, Flex, Text } from "@mantine/core"
+import { useState } from "react"
 
-export default function Pronouns () {
+export default function Pronoun () {
+  const [arrayPronoun] = useState<string[]>([
+    'i', 'he', 'she', 'you', 'we', 'they', 'it'
+  ])
+
   return (
-    <Text>Pronouns Page</Text>
+    <>
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Text>Pronoun</Text>
+        <Text>{arrayPronoun.length}</Text>
+      </Flex>
+
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        {
+          arrayPronoun.map((pronoun, i) => (
+            <Button key={i}>{pronoun}</Button>
+          ))
+        }
+      </Flex>
+    </>
   )
 }
