ce2d8c6e65f8b9c59434877317b8c75144830864
A1exsandro <alexsandro.acp@gmail.com>
2023-03-02 14:50:17 -0400

    content article

--- src/pages/grammar/article.tsx
+++ src/pages/grammar/article.tsx
@@ -1,7 +1,41 @@
-import { Text } from "@mantine/core"
+import { Button, Flex, Text } from "@mantine/core"
+import { useState } from "react"
 
 export default function Article () {
+  const [arrayArtigle] = useState<string[]>([
+    'the', 'a', 'an'
+  ])
+
   return (
-    <Text>Article Page</Text>
+    <>
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        <Text>Article</Text>
+        <Text>{arrayArtigle.length}</Text>
+      </Flex>
+
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="center"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+        {
+          arrayArtigle.map((article, i) => (
+            <Button key={i}>{article}</Button>
+          ))
+        }
+      </Flex>
+    </>
   )
 }
