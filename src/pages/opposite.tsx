8c93e507e189b4ac5b45b1c1e662e666becacf1a
A1exsandro <alexsandro.acp@gmail.com>
2023-03-07 19:33:26 -0400

    add opposite page

---
+++ src/pages/opposite.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react"
+import { Box, Button, Flex, Group, Modal, TextInput } from "@mantine/core"
+import { useForm } from '@mantine/form'
+
+export default function Opposite () {
+  interface NewWord {
+    word: string;
+    opposite: string;
+    first: boolean;
+  }
+
+  const [opened, setOpened] = useState(false)
+  const [buttonText, setButtonText] = useState<string[]>([])
+  const [newWords, setNewWords] = useState([
+    {
+      word: 'Write',
+      opposite: 'Read',
+      first: true
+    },
+    {
+      word: 'White',
+      opposite: 'Black',
+      first: true
+    },
+    {
+      word: 'Tal',
+      opposite: 'Short',
+      first: true
+    },
+    {
+      word: 'After',
+      opposite: 'Before',
+      first: true
+    },
+  ]) 
+ 
+  // Change content button
+  const handleClick = (i: number, nw: NewWord) => {
+    const newButtonText = [...buttonText]
+    nw.first = !nw.first
+    newButtonText[i] = nw.first === true ? nw.word : nw.opposite
+    setButtonText(newButtonText) 
+  }
+
+  const form = useForm({
+    initialValues: {
+      word: '',
+      opposite: '',
+      first: true
+    }
+  })
+
+  return (
+    <>
+      <Group position="right"> 
+        <Button onClick={() => setOpened(true)}>Add Word</Button>
+      </Group>
+      
+     <Modal
+        opened={opened}
+        onClose={() => setOpened(false)} 
+      >
+        <Box sx={{ maxWidth: 300 }} mx="auto">
+          <form onSubmit={form.onSubmit((values) => setNewWords([...newWords, values]))}>
+            <TextInput 
+              label="New Word"
+              placeholder="word"
+              {...form.getInputProps('word')}
+            />
+
+            <TextInput 
+              label="Opposite"
+              placeholder="Opposite"
+              {...form.getInputProps('opposite')}
+            />
+            
+            <Group position="right" mt="md">
+              <Button type="submit" onClick={() => setOpened(false)}>New Word</Button>
+            </Group>
+          </form>
+        </Box>
+      </Modal>
+
+      {/* NEW WORDS */}
+      <Flex
+        mih={50}
+        bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="flex-start"
+        align="center"
+        direction="row"
+        wrap="wrap"
+      >
+      {
+        newWords.map((nw, i) => ( 
+          <Button 
+            key={i}
+            color={nw.first === true ? 'orange' : 'lime'} 
+            onClick={() => handleClick(i, nw)}
+          >  
+            {buttonText[i] || nw.word}
+          </Button>
+        ))
+      }
+      </Flex>
+    </>
+  )
+}
