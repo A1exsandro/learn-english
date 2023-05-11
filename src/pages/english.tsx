2819118edd295a870c76cacd1918033e55aab83e
A1exsandro <alexsandro.acp@gmail.com>
2023-05-11 15:48:58 -0400

    feat: add audio for words

--- src/pages/english.tsx
+++ src/pages/english.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react"
-import { Box, Button, Flex, Group, Modal, TextInput } from "@mantine/core"
+import React, { useRef, useState } from "react"
+import { Box, Button, Flex, Group, Image, Modal, TextInput } from "@mantine/core"
 import { useForm } from '@mantine/form'
+
+import { database, ref, set, get, push, onValue } from "../database/config_database"
+import { child, DataSnapshot } from "firebase/database" 
 
 export default function English () {
   interface NewWord {
     word: string;
     translation: string;
     english: boolean;
+    image?: string;
+    audio?: string;
+  }
+
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play()
+    }
   }
 
   const [opened, setOpened] = useState(false)
   const [buttonText, setButtonText] = useState<string[]>([])
+  const [databaseWord, setDatabaseWord] = useState<string[]>([])
   const [newWords, setNewWords] = useState([
     {
       word: 'Rice',
@@ -33,6 +47,23 @@ export default function English () {
       english: true
     },
   ]) 
+
+  function readData() {
+    const dbRef = ref(database)
+    get(child(dbRef, "/word")).then((snapshot: DataSnapshot) => {
+      if (snapshot.exists()) {
+        //setDatabaseWord(snapshot.val().Alexsandro.newWord)
+        console.log(
+          snapshot.val().pir5HWose7WXMSWBFvrjoqbkxE83.nameWord
+        )
+      } else {
+        console.log("no data available")
+      }
+    }).catch((error:any) => {
+      console.error(error)
+    })
+  } 
+  // console.log(databaseWord)
  
   // Change content button
   const handleClick = (i: number, nw: NewWord) => {
@@ -46,14 +77,18 @@ export default function English () {
     initialValues: {
       word: '',
       translation: '',
-      english: true
+      english: true,
+      image: '',
+      audio: ''
     }
   })
+  console.log(newWords)
 
   return (
     <>
       <Group position="right"> 
         <Button onClick={() => setOpened(true)}>Add Word</Button>
+        <Button onClick={readData}>Get Database</Button>
       </Group>
       
      <Modal
@@ -72,6 +107,18 @@ export default function English () {
               label="Translation"
               placeholder="translation"
               {...form.getInputProps('translation')}
+            />
+
+            <TextInput 
+              label="Image"
+              placeholder="image"
+              {...form.getInputProps('image')}
+            />
+
+            <TextInput 
+              label="Audio"
+              placeholder="audio"
+              {...form.getInputProps('audio')}
             />
             
             <Group position="right" mt="md">
@@ -103,6 +150,29 @@ export default function English () {
         ))
       }
       </Flex>
+
+      <Flex
+        direction="column" 
+        maw={240}
+      >
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/letmeask-333417.appspot.com/o/guava.jpg?alt=media&token=f99814cc-c0e7-425d-b028-fa16e73afe73"
+          height={220}
+          radius="md"
+          mx="auto"
+          fit="contain" 
+          alt="Norway"
+        /> 
+
+        <audio ref={audioRef}>
+          <source src="https://firebasestorage.googleapis.com/v0/b/letmeask-333417.appspot.com/o/guava.mp3?alt=media&token=9954a02d-e0cd-4010-b78d-3f91809b9391" />
+        </audio>
+       
+        <Button onClick={playAudio} variant="light" color="blue" fullWidth mt="md" radius="md">
+          Guava
+        </Button>
+      </Flex>
+
     </>
   )
 }
