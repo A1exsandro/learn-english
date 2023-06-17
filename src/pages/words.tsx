4620b0d6f40a932104b8b7489d3dd11b15441c8c
A1exsandro <alexsandro.acp@gmail.com>
2023-06-17 16:06:24 -0400

    feat: conect with firebase

--- src/pages/words.tsx
+++ src/pages/words.tsx
@@ -1,7 +1,63 @@
 import { Text } from "@mantine/core"
 
+import { ref, getDownloadURL } from "firebase/storage"
+import { useEffect, useState } from "react"
+import { getStorage } from '../services/firebase'
+const storage = getStorage() 
+
 export default function Words () {
+  // GET DATA FROM FIREBASE
+  const [images, setImages] = useState<String[]>([])
+  const [sounds, setSounds] = useState<String[]>([])
+
+  const data = [
+    'butterfly',
+    'avocado',
+    'banana',
+    'bathroom',
+    'bedroom',
+    'breakfast',
+    'bridge',
+    'brothers', 
+    'closed',
+    'cook',
+    'dance',
+    'eat',
+    'fear',
+    'fire',
+    'film',
+    'fruits',
+    'kitchen',
+    'night',
+    'read',
+    'open',
+    'marketplace',
+    'play',
+    'smell',
+    'sleep',
+    'plant',
+    'tired'
+  ] 
+
+  useEffect(() => {
+  const promises = data.map((dt) => (
+    getDownloadURL(ref(storage, `images/${dt}.jpeg`))
+  ))
+
+  const audioPromises = data.map((dt) => (
+    getDownloadURL(ref(storage, `audio/${dt}.mp3`))
+  ))
+
+  Promise.all(promises)
+    .then((urls) => setImages(urls)) 
+
+  Promise.all(audioPromises)
+    .then((audios) => setSounds(audios)) 
+
+  },[data])
+
+  console.log(images)
   return (
     <Text>Words Page</Text>
   )
-}
+} 
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app" 
+import { getDatabase, ref, set, get, push, onValue } from "firebase/database" 
+import { getStorage } from "firebase/storage" 
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -18,5 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig) 
+const database = getDatabase(app) 
+
+export { database, ref, set, get, push, onValue, getStorage }
